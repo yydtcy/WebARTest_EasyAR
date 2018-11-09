9df99120044615a4b91200c061c6e5722361d98b
Ren Zijian <you@example.com>
2018-11-09 14:27:14 +0800

    提交信息

--- html/TokenThreeJsExample/asset/js/main.js
+++ html/TokenThreeJsExample/asset/js/main.js
@@ -12,7 +12,7 @@ function initRender() {
 var camera;
 function initCamera() {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 3000);
-    camera.position.set(0, 0, 50);
+    camera.position.set(0, 0, 5);
     camera.lookAt(new THREE.Vector3(0,0,0));
    //camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
    //camera.position.z = 1000;

4221a891072efbb578042aba0f6633a43e3b4ef2
Ren Zijian <you@example.com>
2018-11-12 11:37:04 +0800

    提交信息

--- html/TokenThreeJsExample/asset/js/main.js
+++ html/TokenThreeJsExample/asset/js/main.js
@@ -82,7 +82,7 @@ function initModel() {
         object.scale.multiplyScalar(0.03);    // 缩放模型大小
         scene.add(object);
     });
-    webAR.trace('over');
+    //webAR.trace('over');
 
     //flag = setInterval(initCube, 1000);
 }

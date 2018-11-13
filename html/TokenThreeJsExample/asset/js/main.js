1847be2a80880298f13e1c1d4ff988859f95f83b
Ren Zijian <you@example.com>
2018-11-13 13:37:01 +0800

    提交信息

--- html/TokenThreeJsExample/asset/js/main.js
+++ html/TokenThreeJsExample/asset/js/main.js
@@ -295,13 +295,13 @@ const MainScene = function(){
         //this.initGui();
         this.initRender();
         //this.initScene();
-        this.initCamera();
-        this.initModel();
-        this.initLight();
-        this.initControls();
-        this.initStats();
+        //this.initCamera();
+        //this.initModel();
+        //this.initLight();
+        //this.initControls();
+        //this.initStats();
     
-        this.animate();
+        //this.animate();
         window.onresize = onWindowResize;
     };
 
